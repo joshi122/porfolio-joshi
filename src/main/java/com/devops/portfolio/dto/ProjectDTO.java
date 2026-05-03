9971104de70c8d74f68026ac3fe6a72e7553e0d2
sudharshan <joshisudarshan068@gmail.com>
2026-05-03 23:29:36 +0530

    Add fields and builder to ProjectDTO

--- src/main/java/com/devops/portfolio/dto/ProjectDTO.java
+++ src/main/java/com/devops/portfolio/dto/ProjectDTO.java
@@ -1,4 +1,20 @@
 package com.devops.portfolio.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String technologies;
+    private String projectUrl;
+    private String githubUrl;
+    private String imageUrl;
 }
